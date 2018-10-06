3bcbcef81263de284e61a2352c2c52cc1550ffdd
hungfc <hungfc.zip@gmail.com>
2018-10-06 21:56:36 +0700

    update news

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,17 @@
 $( document ).ready(function() {
   
   //Hung code here
-
+  $('.new-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    // centerMode: true,
+    // centerPadding: '60px',
+    dots: false,
+    arrows: false
+    // variableWidth: true
+  });
+          
 
 
   //Tuan code here
